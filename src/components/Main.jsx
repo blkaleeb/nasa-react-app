73edf2442d9fdd08e015e8277818a82907587bb7
caleb ignatius <caleb.ignatius@germs.sg>
2024-09-20 11:06:48 +0700

    fix css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Main({ data }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col max-h-screen h-screen w-screen">
       <img
         className="w-full h-full flex-1 object-cover"
         src={data?.hdurl}
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function SideBar({ handleToggleModal, data }) {
   return (
-    <div className="fixed inset-0 sm:relative sm:min-w-[30%] sm:max-h-screen flex flex-col z-10 shadow-sm shadow-[0_0_40px_1px_#030615]">
+    <div className="fixed inset-0 sm:relative sm:min-w-[30%] sm:max-h-full flex flex-col z-10 shadow-sm shadow-[0_0_40px_1px_#030615]">
       <div className="absolute inset-0 bg-[#030615] opacity-60"></div>
-      <div className="z-20 relative flex flex-col gap-4 p-4 overflow-y-scroll ml-auto sm:ml-0 h-full w-11/12 max-w-[800px] bg-[#030615]">
+      <div className="z-20 relative flex flex-col gap-4 p-4 overflow-y-scroll ml-auto sm:ml-0 h-full w-11/12 md:w-full max-w-[800px] bg-[#030615]">
         <h2 className="text-2xl font-light">{data?.title}</h2>
         <div className="flex-1 flex flex-col gap-2">
           <p className="text-lg font-extralight">{data?.date}</p>
